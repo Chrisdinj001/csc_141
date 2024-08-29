5a9f71b7e1bde6ce1dac2fe2ad945991ab5d1513
Chrisdinj001 <chrisdinjones@gmail.com>
2024-08-29 10:45:13 -0400

    First Commit

---
+++ 01_02_hello_world_typos.py
@@ -0,0 +1,3 @@
+# This program will give an error
+print ("Hello World")
+print ("Hello World", ty)
---
+++ 01_03_infinite_skills.py
@@ -0,0 +1,7 @@
+# These are the skills of programming i want tho most
+# In this course I would like to learn different programming langauges.
+# Different coding tasks require specefic programming langauges, knowing 
+# them can open up different opprotunities for me. I would also want to learn
+# password cracking just to add to my bag of skills, I wouldnt use this skill
+# with malicious intent. Finally I would want to learn how to debug things.
+# Debugging helps programmers write better code and get programs working  properly.
