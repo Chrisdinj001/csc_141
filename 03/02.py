1af032bc78ada2a922b6f79e61714a2fa82bce5b
Chrisdinj001 <chrisdinjones@gmail.com>
2024-09-10 10:50:08 -0400

    assignments

--- 03/02.py
+++
@@ -1,1 +0,0 @@
-# This is for lesson 2
---
+++ 03_introducing_list/03_04_guest_list.py
@@ -0,0 +1,18 @@
+guests= ['mariah the scientist','gio','teyana taylor','kehlani']
+
+name= guests[0].title()
+print(f"{name}, your're invited to my dinner.")
+
+name= guests[1].title()
+print(f"{name}, your're invited to my dinner.")
+
+name= guests[2].title()
+print(f"{name}, your're invited to my dinner.")
+
+name= guests[3].title()
+print(f"{name}, your're invited to my dinner.")
+
+
+
+
+
