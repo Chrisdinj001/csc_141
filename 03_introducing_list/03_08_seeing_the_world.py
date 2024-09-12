31408e5f48e4419828561ee2ec817534a990cc5f
Chrisdinj001 <chrisdinjones@gmail.com>
2024-09-12 10:50:54 -0400

    chap 3

---
+++ 03_introducing_list/03_08_seeing_the_world.py
@@ -0,0 +1,28 @@
+locations=['bahamas','turks and cacios' ,'dubai','bali','bora bora']
+
+print("Oringinal order:")
+print(locations)
+
+print("\nAlaphabetical:")
+print(sorted(locations))
+
+print("\nOrignal order:")
+print(locations)
+
+print("\nReverse alphabetical:")
+print(sorted(locations, reverse=True))
+
+print("\nOrignal order:")
+print(locations)
+
+print("Reversed:")
+locations.reverse()
+print(locations)
+
+print("\nOriginal order:")
+locations.reverse()
+print(locations)
+
+print("\nReverse alphabetical")
+locations.sort(reverse=True)
+print(locations)
