074b82d80821c1fc9ec1743989dd8e876ecea220
Chrisdinj001 <chrisdinjones@gmail.com>
2024-09-12 10:26:30 -0400

    chapter 3

---
+++ 03_introducing_list/03_05_changing_guest_list.py
@@ -0,0 +1,33 @@
+
+guests= ['mariah the scientist','gio','teyana taylor','kehlani']
+
+name= guests[0].title()
+print(f"{name}, your're invited to my dinner.")
+
+name= guests[1].title()
+print(f"{name}, your're invited to my dinner.")
+
+name= guests[2].title()
+print(f"{name}, your're invited to my dinner.")
+
+name= guests[3].title()
+print(f"{name}, your're invited to my dinner.")
+
+name= guests[3].title()
+print(f'\nSorry,{name} cant make it to dinner.')
+
+del(guests[3])
+guests.insert(3,"sza")
+
+name= guests[0].title()
+print(f"\n{name}, your're invited to my dinner.")
+
+name= guests[1].title()
+print(f"{name}, your're invited to my dinner.")
+
+name= guests[2].title()
+print(f"{name}, your're invited to my dinner.")
+
+name= guests[3].title()
+print(f"{name}, your're invited to my dinner.")
+
---
+++ 03_introducing_list/03_06_more_guest.py
@@ -0,0 +1,58 @@
+guests= ['mariah the scientist','gio','teyana taylor','kehlani']
+
+name= guests[0].title()
+print(f"{name}, your're invited to my dinner.")
+
+name= guests[1].title()
+print(f"{name}, your're invited to my dinner.")
+
+name= guests[2].title()
+print(f"{name}, your're invited to my dinner.")
+
+name= guests[3].title()
+print(f"{name}, your're invited to my dinner.")
+
+name= guests[3].title()
+print(f'\nSorry,{name} cant make it to dinner.')
+
+del(guests[3])
+guests.insert(3,"sza")
+
+name= guests[0].title()
+print(f"\n{name}, your're invited to my dinner.")
+
+name= guests[1].title()
+print(f"{name}, your're invited to my dinner.")
+
+name= guests[2].title()
+print(f"{name}, your're invited to my dinner.")
+
+name= guests[3].title()
+print(f"{name}, your're invited to my dinner.")
+
+
+# there is a bigger table
+print("\nTheres a bigger table!")
+guests.insert(0,'summer walker')
+guests.insert(2,'beyonce')
+guests.append('nicki minaj')
+
+name= guests[0].title()
+print(f"{name}, you're invited to my dinner.")
+
+name= guests[1].title()
+print(f"{name}, you're invited to my dinner.")
+
+name= guests[2].title()
+print(f"{name}, you're invited to my dinner.")
+
+name= guests[3].title()
+print(f"{name}, you're invited to my dinner.")
+
+name= guests[4].title()
+print(f"{name},you're invited to my dinner")
+
+name= guests[5].title()
+print(f"{name},you're invited to my dinner")
+
+
