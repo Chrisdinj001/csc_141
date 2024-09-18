bacaec5fe82d2d084854ed090afb5d82a51d6aaf
Chrisdinj001 <chrisdinjones@gmail.com>
2024-09-18 09:42:05 -0400

    assigmenta

---
+++ 04_working_with_list/04_01_pizzas.py
@@ -0,0 +1,9 @@
+favorite_pizzas= ['cheese', 'pepporoni','buffalo chicken']
+
+for pizza in favorite_pizzas:
+    print(pizza)
+
+print('\n')
+for pizza in favorite_pizzas:
+    print(f"I really love {pizza} pizza.")
+print("\nI really love pizza!")
---
+++ 04_working_with_list/04_02_animals.py
@@ -0,0 +1,8 @@
+animals=[ 'dog', 'deer','cows']
+for animal in animals:
+    print(animal)
+    
+print("\n")
+for animal in animals:
+    print(f"A {animal} has a wet nose.")
+print("\nAll of these animals have wet noses.")
