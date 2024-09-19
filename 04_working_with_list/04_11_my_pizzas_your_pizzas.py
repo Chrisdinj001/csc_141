4da6bba3a842f52e057e03225a2bca471534422a
Chrisdinj001 <chrisdinjones@gmail.com>
2024-09-19 10:50:41 -0400

    chapter 4

---
+++ 04_working_with_list/04_11_my_pizzas_your_pizzas.py
@@ -0,0 +1,13 @@
+favorite_pizzas= ['cheese', 'pepporoni','buffalo chicken']
+friend_pizzas= favorite_pizzas[:]
+
+favorite_pizzas.append("sausage")
+friend_pizzas.append("spinach")
+
+print('My favorite pizzas are:')
+for pizza in favorite_pizzas;
+    print(f"-{pizza}")
+
+print("\nMy friends favorite pizzas are:")
+for pizza in friend_pizzas:
+        print(f"-{pizza}")
