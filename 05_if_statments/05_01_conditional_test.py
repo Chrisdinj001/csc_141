54c91518feb01ab358db49b81cbb9a7d3c55e5a7
Chrisdinj001 <chrisdinjones@gmail.com>
2024-09-27 09:48:44 -0400

    new assignments

---
+++ 05_if_statments/05_01_conditional_test.py
@@ -0,0 +1,35 @@
+device = 'phone'
+print("Is device == 'phone'? I predict True.")
+print(device == 'phone')
+print("\nIs device == 'tablet'? I predict False.")
+print(device == 'tablet')
+
+car = 'honda'
+print("Is car == 'honda'? I predict True.")
+print(car == 'honda')
+print("\nIs car == 'kia'? I predict False.")
+print(device == 'kia')
+
+
+fruit = 'apple'
+print("Is fruit == 'apple'? I predict True.")
+print(fruit == 'banana')
+print("\nIs fruit == 'banana'? I predict False.")
+print(fruit == 'banana')
+
+
+veggie = 'broccli'
+print("Is veggie == 'broccli'? I predict True.")
+print(veggie == 'asparagus')
+print("\nIs veggie == 'asparagus'? I predict False.")
+print(veggie == 'asparagus')
+
+
+
+name = 'sara'
+print("Is name == 'sara'? I predict True.")
+print(name == 'will')
+print("\nIs name == 'will'? I predict False.")
+print(name == 'will')
+
+
